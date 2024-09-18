cdd59b3f79d01f6d3b0e8fa1e39fd58fbb94d94f
Daniel Rode <2867646+danielrode@users.noreply.github.com>
2024-09-18 08:48:35 -0600

    Update lidar-chm.py to use JSON for PDAL pipeline

--- lidar-chm.py
+++ lidar-chm.py
@@ -6,10 +6,11 @@
 #   pdal 2.6.3
 #   gdal
 # Created: 17 Sep 2024
-# Updated: -
+# Updated: 18 Sep 2024
 
 
 import sys
+import json
 import subprocess as sp
 from sys import exit
 from pathlib import Path
@@ -18,46 +19,44 @@ from datetime import datetime
 
 HELP_TEXT = "Usage: lidar-chm.py  IN_LAS_PATH  OUT_TIFF_PATH"
 
-PDAL_PIPELINE = """[
-  "<IN_FILE_PATH>",
-  // Height normalize
-  {
-    "type":"filters.hag_delaunay"
-  },
-  {
-    "type":"filters.ferry",
-    "dimensions":"HeightAboveGround=>Z"
-  },
-  // Select first returns and points above 2 meters (assuming your CRS using
-  // meters as its unit)
-  //  {
-  //    "type": "filters.expression",
-  //    "expression": "(Z > 2) && (NumberOfReturns > 1 && ReturnNumber == 1)"
-  //  },
-  // Values are set to zero (instead of being filtered out) so GDAL does not
-  // error out on empty pixels)
-  {
-    "type": "filters.assign",
-    "value": [
-      "Z = 0 WHERE Z < 2",
-      "Z = 0 WHERE (NumberOfReturns > 1 && ReturnNumber != 1)"
-    ]
-  },
-  // Generate CHM
-  {
-    "resolution": 0.5,
-    "binmode": true,
-    "dimension": "Z",
-    "output_type": ["max"],
-    "gdaldriver": "GTiff",
-    "filename": "<OUT_FILE_PATH>"
-   }
-]"""
-
 
 def pdal(in_path, out_path):
-  pipeline = PDAL_PIPELINE.replace('<IN_FILE_PATH>', str(in_path))
-  pipeline = pipeline.replace('<OUT_FILE_PATH>', str(out_path))
+  pipeline = [
+    str(in_path),
+    # Height normalize
+    {
+      "type":"filters.hag_delaunay",
+    },
+    {
+      "type":"filters.ferry",
+      "dimensions":"HeightAboveGround=>Z",
+    },
+    # Select first returns and points above 2 meters (assuming your CRS using
+    # meters as its unit)
+    #  {
+    #    "type": "filters.expression",
+    #    "expression": "(Z > 2) && (NumberOfReturns > 1 && ReturnNumber == 1)"
+    #  },
+    # Values are set to zero (instead of being filtered out) so GDAL does not
+    # error out on empty pixels)
+    {
+      "type": "filters.assign",
+      "value": [
+        "Z = 0 WHERE Z < 2",
+        "Z = 0 WHERE (NumberOfReturns > 1 && ReturnNumber != 1)",
+      ],
+    },
+    # Generate CHM
+    {
+      "resolution": 0.5,
+      "binmode": True,
+      "dimension": "Z",
+      # Available options: min, max, mean, idw, count, stdev, and all
+      "output_type": ["max"],
+      "gdaldriver": "GTiff",
+      "filename": str(out_path),
+     }
+  ]
   cmd = [
     "pdal",
     "pipeline",
@@ -66,7 +65,7 @@ def pdal(in_path, out_path):
     "--progress", "./pdal.log",
   ]
   Path("./pdal.log").touch()
-  sp.run(cmd, text=True, check=True, input=pipeline)
+  sp.run(cmd, text=True, check=True, input=json.dumps(pipeline))
 
 
 # Parse command line arguments
